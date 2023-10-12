241399aa8f5f0affbc095218ec08603b010160e3
behemothbucket <mahagon.here@gmail.com>
2023-10-12 19:47:36 +0300

    fix: getDigitsSum refactor

--- src/main/java/utils/MyMath.java
+++ src/main/java/utils/MyMath.java
@@ -3,18 +3,14 @@ package utils;
 public class MyMath {
 
     public static int getDigitsSum(final int i) {
-        int src;
-        if (i < 0) {
-            src = i * -1;
-        } else {
-            src = i;
+        int res = 0;
+        int temp = Math.abs(i);
+
+        for (int j = 0; j < 4; j++) {
+            res += temp % 10;
+            temp = temp / 10;
         }
-        int sum = 0;
-        final String numberAsString = String.valueOf(src);
-        final String[] digitsAsString = numberAsString.split("");
-        for (String stringDigit : digitsAsString) {
-            sum += Integer.parseInt(stringDigit);
-        }
-        return sum;
+
+        return res;
     }
 }
